92ac176f304c15e28a804476f47a0727215170f3
Vikram Kumar <143491161+Vikram8084@users.noreply.github.com>
2024-02-29 00:23:20 +0530

    Added Solution - GfG to GitHub

---
+++ School/Odd or Even/odd-or-even.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution{   
+public:
+    string oddEven(int N){
+        // code here 
+        string s1="odd";
+        string s2="even";
+        if(N%2==0){
+        return s2;
+        }
+        else{
+        return s1;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin >> N;
+        Solution ob;
+        cout << ob.oddEven(N) << endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
