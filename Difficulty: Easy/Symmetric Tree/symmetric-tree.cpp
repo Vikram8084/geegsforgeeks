cd31ecb7efe97ab6689dc1607d23f969793ab139
Vikram Kumar <143491161+Vikram8084@users.noreply.github.com>
2025-06-29 19:55:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Symmetric Tree/symmetric-tree.cpp
@@ -0,0 +1,28 @@
+/*
+class Node {
+public:
+    int data;
+    Node *left, *right;
+
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};
+*/
+
+class Solution {
+public:
+    bool isSymmetric(Node* root) {
+        if (!root) return true;
+        return isMirror(root->left, root->right);
+    }
+
+    bool isMirror(Node* t1, Node* t2) {
+        if (!t1 && !t2) return true;
+        if (!t1 || !t2) return false;
+        return (t1->data == t2->data)
+            && isMirror(t1->left, t2->right)
+            && isMirror(t1->right, t2->left);
+    }
+};
