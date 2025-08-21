927db7be5040c44c51ad5e541cd10d1acef968af
Vikram Kumar <143491161+Vikram8084@users.noreply.github.com>
2025-08-21 22:42:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/First Repeating Element/first-repeating-element.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    int firstRepeated(vector<int> &arr) {
+        // code here
+        unordered_map<int,int>mpp;
+        for(int num:arr){
+            mpp[num]++;
+        }
+        
+        for(int i=0;i<arr.size();i++){
+            if(mpp[arr[i]]>1){
+                return i+1;
+            }
+        }
+        return -1;
+    
+    }
+};
